81130074c56c49e85559f11a7fe544adbf0e057c
ritam-g <ritammaty2006@gmail.com>
2025-12-19 19:37:19 +0530

    today i have learned about components and props

---
+++ COMPONENTS-LERNING-FOLDER/vite-project-componentes/src/App.jsx
@@ -0,0 +1,14 @@
+import Card from './components/card';
+export default function App() {
+  return (
+    <>
+      
+      <Card user="ritam" />
+      <Card user="rohit" />
+      <Card user="rocky" />
+      <Card user="roman" />
+      
+    </>
+  )
+}
+
---
+++ COMPONENTS-LERNING-FOLDER/vite-project-componentes/src/components/card.jsx
@@ -0,0 +1,13 @@
+
+function card(props) {
+    
+  return (
+    
+    
+    <div className="card">
+      <h1>User is {props.user}</h1>
+    </div>
+  )
+}
+
+export default card
