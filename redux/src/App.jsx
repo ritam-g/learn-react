5045527783bda5efadad92a46f34b022174037f4
ritam-g <ritammaty2006@gmail.com>
2026-01-15 17:29:59 +0530

    lerning on redux

--- redux/src/App.jsx
+++ redux/src/App.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './features/counter/counter'
 
-function App() {
+
+export function App() {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
   return (
     <div>
-      <h1>hello</h1>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
     </div>
   )
 }
-
 export default App
